2ce55f6b42fa2df82c50432040a6ce8c8662537a
Jephthah Sadare <jephsadare@gmail.com>
2015-04-21 13:04:29 +0100

    Refactored Address, EmailAddress and PhoneNumber

--- src/org/greenpole/entity/model/Address.java
+++ src/org/greenpole/entity/model/Address.java
@@ -14,14 +14,14 @@ import javax.xml.bind.annotation.XmlType;
 /**
  *
  * @author Yusuf Samsudeen Babashola (Algorithm)
- * Carries the client company address.
+ * Carries the client entity address.
  */
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(propOrder = {"primaryAddress","addressLine","addressLine",
     "addressLine","addressLine","postCode","city","state","country"})
 public class Address {
-    private int clientCompanyId;
+    private int entityId;
     @XmlElement
     private String addressLine1;
     @XmlElement
@@ -42,22 +42,22 @@ public class Address {
     private boolean primaryAddress;
 
     /**
-     * Initialises client company address object.
+     * Initialises client entity address object.
      */
     public Address() {
     }
 
     /**
-     * Collects all data for client company address object.
+     * Collects all data for client entity address object.
      * @param addressLine1 the first address line
      * @param addressLine2 the second address line
      * @param addressLine3 the third address line
      * @param addressLine4 the fourth address line
-     * @param postCode the the company's post code
-     * @param city the the company's resident city
-     * @param state the the company's resident state
-     * @param country the the company's resident country
-     * @param primaryAddress the primary status of the company's address
+     * @param postCode the the entity's post code
+     * @param city the the entity's resident city
+     * @param state the the entity's resident state
+     * @param country the the entity's resident country
+     * @param primaryAddress the primary status of the entity's address
      */
     public Address(String addressLine1, String addressLine2, String addressLine3, 
             String addressLine4, String postCode, String city, String state, String country, boolean primaryAddress) {
@@ -73,23 +73,23 @@ public class Address {
     }
 
     /**
-     * Collects all data for client company address object.
-     * This constructor should be used when trying to submit a client company address for
-     * editing, as it includes the {@link #clientCompanyId} variable, which is not used when
-     * creating a client company address.
-     * @param clientCompanyId the client company id
+     * Collects all data for client entity address object.
+     * This constructor should be used when trying to submit a client entity address for
+     * editing, as it includes the {@link #entityId} variable, which is not used when
+     * creating a client entity address.
+     * @param entityId the client entity id
      * @param addressLine1 the first address line
      * @param addressLine2 the second address line
      * @param addressLine3 the third address line
      * @param addressLine4 the fourth address line
-     * @param postCode the the company's post code
-     * @param city the the company's resident city
-     * @param state the the company's resident state
-     * @param country the the company's resident country
-     * @param primaryAddress the primary status of the company's address
+     * @param postCode the the entity's post code
+     * @param city the the entity's resident city
+     * @param state the the entity's resident state
+     * @param country the the entity's resident country
+     * @param primaryAddress the primary status of the entity's address
      */
-    public Address(int clientCompanyId, String addressLine1, String addressLine2, String addressLine3, String addressLine4, String postCode, String city, String state, String country, boolean primaryAddress) {
-        this.clientCompanyId = clientCompanyId;
+    public Address(int entityId, String addressLine1, String addressLine2, String addressLine3, String addressLine4, String postCode, String city, String state, String country, boolean primaryAddress) {
+        this.entityId = entityId;
         this.addressLine1 = addressLine1;
         this.addressLine2 = addressLine2;
         this.addressLine3 = addressLine3;
@@ -102,17 +102,17 @@ public class Address {
     }
 
     /**
-     * @return the client company id
+     * @return the client entity id
      */
-    public int getClientCompanyId() {
-        return clientCompanyId;
+    public int getEntityId() {
+        return entityId;
     }
 
     /**
-     * @param clientCompanyId the client company id to set
+     * @param entityId the client entity id to set
      */
-    public void setClientCompanyId(int clientCompanyId) {
-        this.clientCompanyId = clientCompanyId;
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
     }
 
     /**
@@ -180,80 +180,80 @@ public class Address {
     }
 
     /**
-     * Gets the company's post code.
-     * @return the company's post code
+     * Gets the entity's post code.
+     * @return the entity's post code
      */
     public String getPostCode() {
         return postCode;
     }
 
     /**
-     * Sets the company's post code.
-     * @param postCode the company's post code
+     * Sets the entity's post code.
+     * @param postCode the entity's post code
      */
     public void setPostCode(String postCode) {
         this.postCode = postCode;
     }
 
     /**
-     * Gets the company's resident city.
-     * @return the company's resident city
+     * Gets the entity's resident city.
+     * @return the entity's resident city
      */
     public String getCity() {
         return city;
     }
 
     /**
-     * Sets the company's resident city.
-     * @param city the company's resident city
+     * Sets the entity's resident city.
+     * @param city the entity's resident city
      */
     public void setCity(String city) {
         this.city = city;
     }
 
     /**
-     * Gets the company's resident state.
-     * @return the company's resident state
+     * Gets the entity's resident state.
+     * @return the entity's resident state
      */
     public String getState() {
         return state;
     }
 
     /**
-     * Sets the company's resident state.
-     * @param state the company's resident state
+     * Sets the entity's resident state.
+     * @param state the entity's resident state
      */
     public void setState(String state) {
         this.state = state;
     }
 
     /**
-     * Gets the company's resident country.
-     * @return the company's resident country
+     * Gets the entity's resident country.
+     * @return the entity's resident country
      */
     public String getCountry() {
         return country;
     }
 
     /**
-     * Sets the company's resident company.
-     * @param country the company's resident company
+     * Sets the entity's resident entity.
+     * @param country the entity's resident entity
      */
     public void setCountry(String country) {
         this.country = country;
     }
 
     /**
-     * Gets the company's primary address status.
-     * @return the company's primary address status
+     * Gets the entity's primary address status.
+     * @return the entity's primary address status
      */
     public boolean isPrimaryAddress() {
         return primaryAddress;
     }
 
     /**
-     * Sets the company's primary address status.
-     * @param primaryAddress the company's primary address status
+     * Sets the entity's primary address status.
+     * @param primaryAddress the entity's primary address status
      */
     public void setPrimaryAddress(boolean primaryAddress) {
         this.primaryAddress = primaryAddress;
---
+++ src/org/greenpole/entity/model/clientcompany/Bank.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenpole.entity.model.clientcompany;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ *
+ * @author Jephthah Sadare
+ */
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType(propOrder = {"id","bankName","bankCode"})
+
+public class Bank {
+    
+    @XmlElement
+    private int id;
+    @XmlElement
+    private String bankName;
+    @XmlElement
+    private String bankCode;
+
+    public Bank(int id, String bankName, String bankCode) {
+        this.id = id;
+        this.bankName = bankName;
+        this.bankCode = bankCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankCode() {
+        return bankCode;
+    }
+
+    public void setBankCode(String bankCode) {
+        this.bankCode = bankCode;
+    }
+    
+    
+    
+}
