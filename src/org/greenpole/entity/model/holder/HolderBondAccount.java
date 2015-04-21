4b2206b0bf19ab265caff6d01db6d12d683aef00
Jephthah Sadare <jephsadare@gmail.com>
2015-04-21 13:48:14 +0100

    Changed package location for Holder, HolderBondAccount and HolderCompanyAccount

--- src/org/greenpole/entity/model/holder/HolderBondAccount.java
+++ src/org/greenpole/entity/model/holder/HolderBondAccount.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.greenpole.entity.model.jeph.models.holder;
+package org.greenpole.entity.model.holder;
 
 import org.greenpole.entity.model.jeph.models.payment.Coupon;
 import org.greenpole.entity.model.jeph.models.payment.BondOffer;
