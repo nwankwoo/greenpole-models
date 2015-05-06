9f415934409f4e2e40424b6be9315fe2a0155c4a
Akin <Akin@10.101.184.157>
2015-05-06 15:07:01 +0100

    Created HolderMerger model for merge

--- src/org/greenpole/entity/model/holder/HolderMerger.java
+++ src/org/greenpole/entity/model/holder/HolderMerger.java
@@ -22,18 +22,24 @@ import javax.xml.bind.annotation.XmlType;
 @XmlRootElement
 @XmlSeeAlso({Holder.class})
 @XmlAccessorType(XmlAccessType.NONE)
-@XmlType(propOrder = {"primaryHolder", "secondaryHolders"})
+@XmlType(propOrder = {"primaryHolder","pryHolderChanges","secondaryHolders"})
 public class HolderMerger {
     @XmlElement
     private Holder primaryHolder;
+    //used to store changes made to the primary holder account as a result of the merge. Form should be:
+    //form should be: type:value;gender:value;dob:value;phone:value;email:value
+    //where "value" could be "none" or the previous value of the variable before its change, NOT the current value the variable holds
+    @XmlElement
+    private String pryHolderChanges;
     @XmlElementWrapper(name = "secondaryHolders")
     private List<Holder> secondaryHolders;
 
     public HolderMerger() {
     }
 
-    public HolderMerger(Holder primaryHolder, List<Holder> secondaryHolders) {
+    public HolderMerger(Holder primaryHolder, String pryHolderChanges, List<Holder> secondaryHolders) {
         this.primaryHolder = primaryHolder;
+        this.pryHolderChanges = pryHolderChanges;
         this.secondaryHolders = secondaryHolders;
     }
 
@@ -43,6 +49,14 @@ public class HolderMerger {
 
     public void setPrimaryHolder(Holder primaryHolder) {
         this.primaryHolder = primaryHolder;
+    }
+
+    public String getPryHolderChanges() {
+        return pryHolderChanges;
+    }
+
+    public void setPryHolderChanges(String pryHolderChanges) {
+        this.pryHolderChanges = pryHolderChanges;
     }
 
     public List<Holder> getSecondaryHolders() {
