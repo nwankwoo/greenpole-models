800c950c7f733e343730a0de90cb66b99c3b0096
Akin <Akin@Lenovo-PC.homerouter.cpe>
2015-06-10 09:04:14 +0100

    Added more notification types

--- src/org/greenpole/entity/notification/NotificationType.java
+++ src/org/greenpole/entity/notification/NotificationType.java
@@ -11,5 +11,24 @@ package org.greenpole.entity.notification;
  * All certified notification types.
  */
 public enum NotificationType {
-    Transpose;
+    create_client_company,
+    edit_client_company,
+    setup_bond_offer,
+    apply_for_bond_offer,
+    setup_ipo,
+    setup_private_placement,
+    upload_unit_quotations,
+    create_shareholder,
+    create_bondholder,
+    edit_holder,
+    upload_holder_signature,
+    create_administrator,
+    upload_power_of_attorney,
+    transpose,
+    merge_accounts,
+    demerge_accounts,
+    transfer_shares,
+    transfer_bonds,
+    store_nuban,
+    change_nuban
 }
