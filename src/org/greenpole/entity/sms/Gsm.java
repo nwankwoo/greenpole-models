37e330f7af08c82bc90f61c663f571eb908a726e
Akin <Akin@192.168.1.2>
2015-06-15 17:08:09 +0100

    Added model for SMS

---
+++ src/org/greenpole/entity/sms/Gsm.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.greenpole.entity.sms;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+/**
+ *
+ * @author Akinwale Agbaje
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType(propOrder = {"messageId","content"})
+public class Gsm {
+    @XmlValue
+    private String content;
+    @XmlAttribute
+    private String messageId;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [content = " + content + ", messageId = " + messageId + "]";
+    }
+}
